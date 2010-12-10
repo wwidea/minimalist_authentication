29bac986a7c459d1413ea77a55f08c213ff3a0f3
Aaron Baldwin <baldwina@wwidea.org>
2010-12-10 11:08:29 -0700

    updates to run under 1.9.2

--- test/authorization_test.rb
+++ test/authorization_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require 'test_helper'
 
 class AuthorizationTest < ActiveSupport::TestCase
   def AuthorizationTest.helper_method(*args); end
--- test/sessions_test.rb
+++ test/sessions_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper'
+require 'test_helper'
 require 'action_controller'
 require 'action_controller/test_process'
 ActionController::Routing::Routes.draw do |map|
