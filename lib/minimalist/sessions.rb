d55125f8805170b85a62e087f6ebe5f573f1efba
Jonathan Garvin <jgarvin@wwidea.org>
2010-12-15 14:28:05 -0700

    add failed authentication callback

--- lib/minimalist/sessions.rb
+++ lib/minimalist/sessions.rb
@@ -23,6 +23,7 @@ module Minimalist
           redirect_back_or_default(login_redirect_to(user))
           return
         else
+          after_authentication_failure
           flash.now[:error] = "Couldn't log you in as '#{params[:email]}'"
           render :action => 'new'
         end
@@ -47,6 +48,10 @@ module Minimalist
       end
       
       def after_authentication(user)
+        # overide in application
+      end
+      
+      def after_authentication_failure(user)
         # overide in application
       end
     end
