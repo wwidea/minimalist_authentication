9cb8ae082897dc3cb0d0a3633cce0b241194cc74
Jonathan Garvin <jgarvin@wwidea.org>
2012-07-18 12:47:22 -0600

    use new salt whenever rehashing password

--- lib/minimalist/authentication.rb
+++ lib/minimalist/authentication.rb
@@ -100,7 +100,7 @@ module Minimalist
       
       def encrypt_password
         return if password.blank?
-        self.salt = self.class.make_token if new_record?
+        self.salt = self.class.make_token
         self.crypted_password = self.class.secure_digest(password, salt, (self.respond_to?(:using_digest_version) ? PREFERRED_DIGEST_VERSION : 1))
         self.using_digest_version = PREFERRED_DIGEST_VERSION if self.respond_to?(:using_digest_version)
       end
