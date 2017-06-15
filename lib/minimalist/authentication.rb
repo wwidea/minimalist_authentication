077f6db8461754dd772cb52e6e4e0e2df3f2ed21
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-06-15 14:50:45 -0600

    update to use ActiveSupport::Concern

--- lib/minimalist/authentication.rb
+++ lib/minimalist/authentication.rb
@@ -3,6 +3,8 @@ require 'bcrypt'
 
 module Minimalist
   module Authentication
+    extend ActiveSupport::Concern
+    
     GUEST_USER_EMAIL = 'guest'
     PREFERRED_DIGEST_VERSION = 3
 
@@ -11,25 +13,18 @@ module Minimalist
     # future for better encryption.
     CALIBRATED_BCRYPT_COST = BCrypt::Engine.calibrate(750)
 
-    def self.included( base )
-      base.extend(ClassMethods)
-      base.class_eval do
-        include InstanceMethods
+    included do
+      attr_accessor :password
+      before_save :encrypt_password
 
-        attr_accessor :password
-        before_save :encrypt_password
+      validates_presence_of     :email, if: :validate_email_presence?
+      validates_uniqueness_of   :email, allow_blank: true, if: :validate_email_uniqueness?
+      validates_format_of       :email, allow_blank: true, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, if: :validate_email_format?
+      validates_presence_of     :password,                 if: :password_required?
+      validates_confirmation_of :password,                 if: :password_required?
+      validates_length_of       :password, within: 6..40,  if: :password_required?
 
-        validates_presence_of     :email, if: :validate_email_presence?
-        validates_uniqueness_of   :email, allow_blank: true, if: :validate_email_uniqueness?
-        validates_format_of       :email, allow_blank: true, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, if: :validate_email_format?
-        validates_presence_of     :password,                 if: :password_required?
-        validates_confirmation_of :password,                 if: :password_required?
-        validates_length_of       :password, within: 6..40,  if: :password_required?
-
-        scope :active, ->(active = true) { where active: active }
-
-
-      end
+      scope :active, ->(active = true) { where active: active }
     end
 
     module ClassMethods
@@ -54,84 +49,78 @@ module Minimalist
       end
 
       def guest
-        new.tap do |user|
-          user.email = GUEST_USER_EMAIL
-        end
+        new(email: GUEST_USER_EMAIL)
       end
     end
 
-    module InstanceMethods
+    def active?
+      active
+    end
 
-      def active?
-        active
-      end
-
-      def authenticated?(password)
-        if crypted_password == encrypt(password)
-          if self.respond_to?(:using_digest_version) and (using_digest_version != PREFERRED_DIGEST_VERSION or salt_cost < CALIBRATED_BCRYPT_COST)
-            new_salt = self.class.make_token
-            self.update_attribute(:crypted_password,self.class.secure_digest(password, new_salt, PREFERRED_DIGEST_VERSION))
-            self.update_attribute(:salt, new_salt)
-            self.update_attribute(:using_digest_version, PREFERRED_DIGEST_VERSION)
-          end
-          return true
-        else
-          return false
+    def authenticated?(password)
+      if crypted_password == encrypt(password)
+        if self.respond_to?(:using_digest_version) && (using_digest_version != PREFERRED_DIGEST_VERSION || salt_cost < CALIBRATED_BCRYPT_COST)
+          new_salt = self.class.make_token
+          self.update_attribute(:crypted_password, self.class.secure_digest(password, new_salt, PREFERRED_DIGEST_VERSION))
+          self.update_attribute(:salt, new_salt)
+          self.update_attribute(:using_digest_version, PREFERRED_DIGEST_VERSION)
         end
+        return true
+      else
+        return false
       end
+    end
 
-      def logged_in
-        update_column(:last_logged_in_at, Time.current) # use update_column to avoid updated_on trigger
-      end
+    def logged_in
+      update_column(:last_logged_in_at, Time.current) # use update_column to avoid updated_on trigger
+    end
 
-      def is_guest?
-        email == GUEST_USER_EMAIL
-      end
+    def is_guest?
+      email == GUEST_USER_EMAIL
+    end
 
-      #######
-      private
-      #######
 
-      def password_required?
-        active? && (crypted_password.blank? || !password.blank?)
-      end
+    private
 
-      def encrypt(password)
-        self.class.secure_digest(password, salt, digest_version)
-      end
+    def password_required?
+      active? && (crypted_password.blank? || !password.blank?)
+    end
 
-      def encrypt_password
-        return if password.blank?
-        self.salt = self.class.make_token
-        self.crypted_password = self.class.secure_digest(password, salt, (self.respond_to?(:using_digest_version) ? PREFERRED_DIGEST_VERSION : 1))
-        self.using_digest_version = PREFERRED_DIGEST_VERSION if self.respond_to?(:using_digest_version)
-      end
+    def encrypt(password)
+      self.class.secure_digest(password, salt, digest_version)
+    end
 
-      def digest_version
-        self.respond_to?(:using_digest_version) ? (using_digest_version || 1) : 1
-      end
+    def encrypt_password
+      return if password.blank?
+      self.salt = self.class.make_token
+      self.crypted_password = self.class.secure_digest(password, salt, (self.respond_to?(:using_digest_version) ? PREFERRED_DIGEST_VERSION : 1))
+      self.using_digest_version = PREFERRED_DIGEST_VERSION if self.respond_to?(:using_digest_version)
+    end
 
-      def salt_cost
-        BCrypt::Engine.valid_salt?(salt) ? salt.match(/\$[^\$]+\$([0-9]+)\$/)[1].to_i : 0
-      end
+    def digest_version
+      self.respond_to?(:using_digest_version) ? (using_digest_version || 1) : 1
+    end
 
-      # email validation
-      def validate_email?
-        # allows applications to turn off email validation
-        true
-      end
+    def salt_cost
+      BCrypt::Engine.valid_salt?(salt) ? salt.match(/\$[^\$]+\$([0-9]+)\$/)[1].to_i : 0
+    end
 
-      def validate_email_presence?
-        validate_email? && active?
-      end
+    # email validation
+    def validate_email?
+      # allows applications to turn off email validation
+      true
+    end
 
-      def validate_email_format?
-        validate_email? && active?
-      end
+    def validate_email_presence?
+      validate_email? && active?
+    end
 
-      def validate_email_uniqueness?
-        validate_email? && active?
-      end
+    def validate_email_format?
+      validate_email? && active?
+    end
+
+    def validate_email_uniqueness?
+      validate_email? && active?
     end
   end
 end
