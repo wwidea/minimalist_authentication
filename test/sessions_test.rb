f510fe2de988bef18d7034ef3af1183ab93fecb9
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-06-15 09:38:16 -0600

    wrap params in a hash

--- test/sessions_test.rb
+++ test/sessions_test.rb
@@ -9,22 +9,21 @@ class SessionsControllerTest < ActionController::TestCase
   
   test "should create session" do
     user = FactoryGirl.create(:user)
-    post :create, email: 'test@testing.com', password: 'password'
-    assert_equal(user.id, session[:user_id])
+    post :create, params: { email: 'test@testing.com', password: 'password' }
+    assert_equal user.id, session[:user_id]
     assert_redirected_to '/'
   end
   
   test "should fail to create session" do
-    user = FactoryGirl.create(:user)
-    post :create, email: 'test@testing.com', password: 'wrong_password'
-    assert_nil(session[:user_id])
+    post :create, params: { email: 'test@testing.com', password: 'wrong_password' }
+    assert_nil session[:user_id]
     assert_response :success
   end
   
   test "should destroy session" do
     @request.session[:user_id] = 1
     delete :destroy
-    assert_nil(session[:user_id])
+    assert_nil session[:user_id]
     assert_redirected_to '/'
   end
 end
