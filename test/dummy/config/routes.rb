1cd5d9298ab98459c9d27d56b815aa43594dfcfc
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-06-15 16:18:45 -0600

    make dummy application useable

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :session, only: %i(new create destroy)
+  resource :dashboard,  only: :show
+  resource :session,    only: %i(new create destroy)
+  
+  root to: 'dashboards#show'
 end
