c2e8a3c23f8810bcfaff67d89ea09aeb8f0e5212
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-06-15 15:13:17 -0600

    update to use ActiveSupport::Concern

--- lib/minimalist/sessions.rb
+++ lib/minimalist/sessions.rb
@@ -1,59 +1,52 @@
 module Minimalist
   module Sessions
-    def self.included( base )
-      base.class_eval do
-        include InstanceMethods
-      end
+    ActiveSupport::Concern
+
+    def show
+      redirect_to new_session_path
     end
     
-    module InstanceMethods
-      def show
-        redirect_to new_session_path
-      end
-      
-      def new
-        @user = User.new
-      end
+    def new
+      @user = User.new
+    end
 
-      def create
-        if user = User.authenticate(params[:email], params[:password])
-          user.logged_in
-          session[:user_id] = user.id
-          after_authentication(user)
-          redirect_back_or_default(login_redirect_to(user))
-          return
-        else
-          after_authentication_failure(user)
-          flash.now[:alert] = "Couldn't log you in as '#{params[:email]}'"
-          render action: 'new'
-        end
+    def create
+      if user = User.authenticate(params[:email], params[:password])
+        user.logged_in
+        session[:user_id] = user.id
+        after_authentication(user)
+        redirect_back_or_default(login_redirect_to(user))
+        return
+      else
+        after_authentication_failure(user)
+        flash.now[:alert] = "Couldn't log you in as '#{params[:email]}'"
+        render action: 'new'
       end
+    end
 
-      def destroy
-        session[:user_id] = nil
-        flash[:notice] = "You have been logged out."
-        redirect_to logout_redirect_to
-      end
-      
-      #######
-      private
-      #######
-      
-      def login_redirect_to(user)
-        '/'
-      end
-      
-      def logout_redirect_to
-        '/'
-      end
-      
-      def after_authentication(user)
-        # overide in application
-      end
-      
-      def after_authentication_failure(user)
-        # overide in application
-      end
+    def destroy
+      session[:user_id] = nil
+      flash[:notice] = "You have been logged out."
+      redirect_to logout_redirect_to
+    end
+
+
+    private
+
+    def login_redirect_to(user)
+      '/'
+    end
+
+    def logout_redirect_to
+      '/'
+    end
+
+    def after_authentication(user)
+      # overide in application
+    end
+
+    def after_authentication_failure(user)
+      # overide in application
     end
   end
 end
