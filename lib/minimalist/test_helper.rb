86eef3b74dcae12e2ec5b6fa86e23f90a4158d1a
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-06-16 09:59:37 -0600

    update to inherit from ActionDispatch::IntegrationTest

--- lib/minimalist/test_helper.rb
+++ lib/minimalist/test_helper.rb
@@ -1,12 +1,12 @@
 module Minimalist
   module TestHelper
-    # Sets the current user in the session from the user fixtures.
-    def login_as(user)
-      @request.session[:user_id] = user ? users(user).id : nil
+    def login_as(user_fixture_name, password = 'password')
+      post session_path, params: { user: { email: users(user_fixture_name).email, password: password } }
     end
     
+    
     def current_user
-      @current_user ||= User.find(@request.session[:user_id])
+      @current_user ||= (@request.session[:user_id] ? User.find(@request.session[:user_id]) : nil)
     end
   end
 end
--- test/sessions_test.rb
+++ test/sessions_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   include Minimalist::TestHelper
   
   test "should get new" do
-    get :new
+    get new_session_path
     assert_response :success
   end
   
   test "should create session" do
-    post :create, params: { user: { email: 'test@testing.com', password: 'password' } }
-    assert_equal users(:active_user).id, session[:user_id]
-    assert_redirected_to '/'
+    post session_path(user: { email: 'test@testing.com', password: 'password' } )
+    assert_equal users(:active_user), current_user
+    assert_redirected_to root_path
   end
   
   test "should fail to create session" do
-    post :create, params: { user: { email: 'test@testing.com', password: 'wrong_password' } }
-    assert_nil session[:user_id]
+    post session_path(user: { email: 'test@testing.com', password: 'wrong_password' } )
+    assert_nil current_user
     assert_response :success
   end
   
   test "should destroy session" do
     login_as :active_user
-    delete :destroy
-    assert_nil session[:user_id]
-    assert_redirected_to '/'
+    delete session_path
+    assert_nil current_user
+    assert_redirected_to root_path
   end
 end
