27706b256a892e42576643fb933460ca02b4ee7d
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-09-12 16:27:10 -0600

    remove spaces

--- lib/minimalist/test_helper.rb
+++ lib/minimalist/test_helper.rb
@@ -3,8 +3,8 @@ module Minimalist
     def login_as(user_fixture_name, password = 'password')
       post session_path, params: { user: { email: users(user_fixture_name).email, password: password } }
     end
-    
-    
+
+
     def current_user
       @current_user ||= (@request.session[:user_id] ? User.find(@request.session[:user_id]) : nil)
     end
