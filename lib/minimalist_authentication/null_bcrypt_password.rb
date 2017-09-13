4ba94a0eadef94482f42e5c42daa286f967ba6e6
Aaron Baldwin <baldwina@wwidea.org>
2017-09-13 14:44:16 -0600

    cost will never be called

--- lib/minimalist_authentication/null_bcrypt_password.rb
+++ lib/minimalist_authentication/null_bcrypt_password.rb
@@ -1,15 +1,8 @@
 module MinimalistAuthentication
   class NullBCryptPassword
-
     # does not match any object
     def ==(object)
       false
     end
-
-    # make cost 0
-    def cost
-      0
-    end
-
   end
 end
