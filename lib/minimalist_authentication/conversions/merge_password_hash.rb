e23979f2d3d1b4eab755658ba660c3dfcbb1becf
Aaron Baldwin <baldwina@wwidea.org>
2017-09-18 12:00:30 -0600

    add merge_password_hash conversion

---
+++ lib/minimalist_authentication/conversions/merge_password_hash.rb
@@ -0,0 +1,38 @@
+module MinimalistAuthentication
+  module Conversions
+    class MergePasswordHash
+
+      class << self
+        def run!
+          user_model.where(using_digest_version: 3, password_hash: nil).each do |user|
+            new(user).update!
+          end
+        end
+
+        private
+
+        def user_model
+          MinimalistAuthentication.configuration.user_model
+        end
+      end
+
+      attr_accessor :user
+
+      delegate :salt, :crypted_password, to: :user
+
+      def initialize(user)
+        self.user = user
+      end
+
+      def update!
+        user.update_column(:password_hash, merged_password_hash)
+      end
+
+      private
+
+      def merged_password_hash
+        "#{salt}#{crypted_password}"
+      end
+    end
+  end
+end
---
+++ test/conversions/merge_password_hash_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class MergePasswordHashTest < ActiveSupport::TestCase
+  def setup
+    password_hash = MinimalistAuthentication::Password.create('password')
+
+    users(:legacy_user).update_columns(
+      using_digest_version: 3,
+      crypted_password:     password_hash.checksum,
+      salt:                 password_hash.salt,
+      password_hash:        nil
+    )
+  end
+
+  test 'should run merge password hash' do
+    assert_difference "User.where(password_hash: nil).count", -1 do
+      assert MinimalistAuthentication::Conversions::MergePasswordHash.run!
+    end
+  end
+
+  test 'should mmerge password hash' do
+    MinimalistAuthentication::Conversions::MergePasswordHash.new(users(:legacy_user)).update!
+    assert BCrypt::Password.new(users(:legacy_user).reload[:password_hash]) == 'password'
+  end
+end
