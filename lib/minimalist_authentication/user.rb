8c15d99601b2b5ba96936060f2ea0d941a5f7a04
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-09-17 21:43:01 -0600

    document authenticate method

--- lib/minimalist_authentication/user.rb
+++ lib/minimalist_authentication/user.rb
@@ -8,23 +8,34 @@ module MinimalistAuthentication
     EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
     included do
+      # Stores the plain text password.
       attr_accessor :password
+
+      # Hashes and stores the password on save.
       before_save :hash_password
 
-      # email validations
+      # Email validations
       validates_presence_of     :email,                                       if: :validate_email_presence?
       validates_uniqueness_of   :email, allow_blank: true,                    if: :validate_email?
       validates_format_of       :email, allow_blank: true, with: EMAIL_REGEX, if: :validate_email?
 
-      # password validations
+      # Password validations
       validates_presence_of     :password,                  if: :validate_password?
       validates_confirmation_of :password,                  if: :validate_password?
       validates_length_of       :password, within: 6..40,   if: :validate_password?
 
+      # Active scope
       scope :active, ->(active = true) { where active: active }
     end
 
     module ClassMethods
+      # Authenticates a user form the params provied. Expects a params hash with
+      # email or username and passwod keys.
+      # Params examples:
+      # { email: 'user@example.com', password: 'abc123' }
+      # { username: 'user', password: 'abc123' }
+      # Returns user upon successful authentcation.
+      # Otherwise returns nil.
       def authenticate(params)
         field, value = params.to_h.select { |key, value| %w(email username).include?(key.to_s) && value.present? }.first
         return if field.blank? || value.blank? || params[:password].blank?
@@ -33,7 +44,7 @@ module MinimalistAuthentication
         return user
       end
 
-      # Retunrs a user with the email set to GUEST_USER_EMAIL.
+      # Retunrs a frozen user with the email set to GUEST_USER_EMAIL.
       def guest
         new(email: GUEST_USER_EMAIL).freeze
       end
