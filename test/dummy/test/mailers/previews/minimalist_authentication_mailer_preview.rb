100f035bb5755037faf9e4c43d6d485e85c3085f
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-09-28 09:45:52 -0600

    move mailer previews to dummy app

--- test/dummy/test/mailers/previews/minimalist_authentication_mailer_preview.rb
+++ test/dummy/test/mailers/previews/minimalist_authentication_mailer_preview.rb
@@ -3,12 +3,16 @@ class MinimalistAuthenticationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/minimalist_authentication_mailer/verify_email
   def verify_email
-    MinimalistAuthenticationMailer.verify_email
+    user = User.find_by(email: 'legacy@example.com')
+    user.regenerate_verification_token
+    MinimalistAuthenticationMailer.verify_email(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/minimalist_authentication_mailer/update_password
   def update_password
-    MinimalistAuthenticationMailer.update_password
+    user = User.find_by(email: 'active@example.com')
+    user.regenerate_verification_token
+    MinimalistAuthenticationMailer.update_password(user)
   end
 
 end
