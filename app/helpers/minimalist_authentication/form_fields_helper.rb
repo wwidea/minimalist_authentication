3df4b07b24ef2c555a4cd7c2fdff4bb532921b3b
Aaron Baldwin <baldwina@brightwayslearning.org>
2024-11-21 07:14:14 -0700

    add options argument

--- app/helpers/minimalist_authentication/form_fields_helper.rb
+++ app/helpers/minimalist_authentication/form_fields_helper.rb
@@ -2,8 +2,16 @@
 
 module MinimalistAuthentication
   module FormFieldsHelper
-    def password_reset_email_field(form)
-      form.email_field(:email, autocomplete: "email", autofocus: true, placeholder: t(".placeholder"), required: true)
+    def password_reset_email_field(form, options = {})
+      form.email_field(
+        :email,
+        options.reverse_merge(
+          autocomplete: "email",
+          autofocus:    true,
+          placeholder:  t(".placeholder"),
+          required:     true
+        )
+      )
     end
   end
 end
